77ef82126a496f5bf39207e964b681b509afdc2e
eth3l <atrnd.work@gmail.com>
2025-09-17 08:58:49 +0200

    ++

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ATrnd-home/', // GitHub repo name
 })
